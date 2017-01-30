9a7e8f42c63e7b313043ac12949d0aa27ac3231f
tgrosch91 <tgrosch91@gmail.com>
2017-01-29 22:51:11 -0600

    finished

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import random
+
+def getRandomFortune():
+
+    fortunes = ["You will make lots of money.", "You will fall in love.", "You will face your worst fear."]
+
+    index = random.randint(0,2)
+
+    return fortunes[index]
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        header = "<h1>FortuneCookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_number= "<strong>" + str(random.randint(1,100)) +"</strong>"
+        number_sentence='Your lucky number: ' + str(lucky_number)
+        number_paragraph="<p>"+number_sentence+"</p>"
+
+        cookie_again_button = "<a href='.'><button>Another cookie please!</button></a>"
+
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+        self.response.write(content)
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
